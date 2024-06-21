8710f9d57dd6f6de1976a0b698769fa71d866eb9
hp <hp@DESKTOP-4JSGQT6>
2024-06-21 10:37:36 +0530

    first sample jenkin example

---
+++ lastproject/src/main/java/jenkinsexample/Sample_Ex.java
@@ -0,0 +1,12 @@
+package jenkinsexample;
+
+public class Sample_Ex {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("FIrst jenkin project");
+
+	}
+
+}
